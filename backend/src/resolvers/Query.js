afe6b6e905dca1d330f19d6e86441afb45c34ca2
Ian Izaguirre <ian@izaguir.re>
2018-10-25 17:13:04 -0400

    Chore: finish items creation and prisma yoga flow

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,16 +1,28 @@
+const { forwardTo } = require('prisma-binding');
+// const Query = {
+//   async items(parent, args, ctx, info) {
+//     const items = await ctx.db.query.items(
+//       {
+//         where: {
+//           title: args.title
+//         }
+//       },
+//       info
+//     );
+//     return items;
+//   }
+// };
+
+// const Query = {
+//   async items(parent, args, ctx, info) {
+//     console.log('Getting Items');
+//     const items = await ctx.db.query.items();
+//     return items;
+//   }
+// };
+
 const Query = {
-  async items(parent, args, ctx, info) {
-    // const items = await ctx.db.query.items();
-    const items = await ctx.db.query.items(
-      {
-        where: {
-          title: args.title
-        }
-      },
-      info
-    );
-    return items;
-  }
+  items: forwardTo('db')
 };
 
 module.exports = Query;
