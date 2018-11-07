576b2f348080a2b3deb2dbce6ed0993e37c8de79
Ian Izaguirre <ian@izaguir.re>
2018-11-06 20:41:12 -0500

    chore: add ability to update items with queries and mutations

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -11,7 +11,24 @@ const Mutations = {
       info
     );
     return item;
+  },
+
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info // info is how this updateItem function knows what to return - which is determined by what we told this function to return in schema.graphql and we told it to return : Item!
+    );
   }
-};
+}; // END OF MUTATIONS OBJECT
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -22,7 +22,8 @@ const { forwardTo } = require('prisma-binding');
 // };
 
 const Query = {
-  items: forwardTo('db')
+  items: forwardTo('db'),
+  item: forwardTo('db'),
 };
 
 module.exports = Query;
